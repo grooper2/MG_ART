ccf00294a303ba60d190b552ab66a438e16c348d
grooper2 <athanasiou454@gmail.com>
2020-08-28 16:40:52 +0100

    2nd commit

--- src/js/mdb.free.js
+++ src/js/mdb.free.js
@@ -7,20 +7,18 @@ import Modal from './bootstrap/src/modal';
 import Popover from './bootstrap/src/popover';
 import ScrollSpy from './bootstrap/src/scrollspy';
 import Tab from './bootstrap/src/tab';
-import Toast from './bootstrap/src/toast';
 import Tooltip from './bootstrap/src/tooltip';
+import Toast from './bootstrap/src/toast';
 
 // MDB FREE COMPONENTS
 import Input from './free/input';
 import Dropdown from './free/dropdown';
-import Treeview from './free/treeview';
 import Ripple from './free/ripple';
 
 // AUTO INIT
-const tooltips = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'));
-const popovers = [].slice.call(document.querySelectorAll('[data-toggle="popover"]'));
-if (tooltips.length > 0) tooltips.map((tooltip) => new Tooltip(tooltip));
-if (popovers.length > 0) popovers.map((popover) => new Popover(popover));
+[...document.querySelectorAll('[data-toggle="tooltip"]')].map((tooltip) => new Tooltip(tooltip));
+[...document.querySelectorAll('[data-toggle="popover"]')].map((popover) => new Popover(popover));
+[...document.querySelectorAll('.toast')].map((toast) => new Toast(toast));
 
 export {
   Alert,
@@ -36,5 +34,4 @@ export {
   Tab,
   Toast,
   Tooltip,
-  Treeview,
 };
